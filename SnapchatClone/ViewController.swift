4ff1a20ab747c91f66695ca50110186c256aa27f
Jimmy Rao <32654532+jimjimrao@users.noreply.github.com>
2023-11-08 20:25:46 -0500

    Initial Commit

---
+++ SnapchatClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SnapchatClone
+//
+//  Created by Jimmy Rao on 11/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
